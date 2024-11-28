089554f6cc0097b03ff9f722713f75c3f899b928
TheHKT <hworschinski@gmail.com>
2024-11-28 15:46:05 +0100

    pt1 of hw imple

--- src/adap-b05/files/Directory.ts
+++ src/adap-b05/files/Directory.ts
@@ -1,3 +1,4 @@
+import { AssertionDispatcher, ExceptionType } from "../common/AssertionDispatcher";
 import { Node } from "./Node";
 
 export class Directory extends Node {
@@ -9,11 +10,77 @@ export class Directory extends Node {
     }
 
     public add(cn: Node): void {
+        //this.assertValidNode(cn, ExceptionType.PRECONDITION);
+        AssertionDispatcher.dispatch(ExceptionType.PRECONDITION, !this.childNodes.has(cn), "Node already exists");
+        let clone: Set<Node> = new Set<Node>(this.childNodes);
+        
         this.childNodes.add(cn);
+
+        this.assertClassInvariants();
+        try{
+            AssertionDispatcher.dispatch(ExceptionType.POSTCONDITION, this.childNodes.has(cn), "Node not added");
+        } catch (e: any) {
+            this.childNodes = clone;
+            throw e;
+        }
     }
 
     public remove(cn: Node): void {
+        this.assertValidNode(cn, ExceptionType.PRECONDITION);
+        AssertionDispatcher.dispatch(ExceptionType.PRECONDITION, this.childNodes.has(cn), "Node dpenst exist as child");
+        let clone: Set<Node> = new Set<Node>(this.childNodes);
+        
         this.childNodes.delete(cn); // Yikes! Should have been called remove
+
+        this.assertClassInvariants();
+        try{
+            AssertionDispatcher.dispatch(ExceptionType.POSTCONDITION, this.childNodes.has(cn), "Node not deleted");
+        } catch (e: any) {
+            this.childNodes = clone;
+            throw e;
+        }
     }
 
+    public getChildNodes(): Set<Node> {
+        let res : Set<Node> = this.doGetChildNodes();
+
+        this.assertClassInvariants();
+        this.assertIsNotNullOrUndefined(res, ExceptionType.POSTCONDITION);
+        return res;
+    }
+    protected doGetChildNodes(): Set<Node> {
+        return this.childNodes;
+    }
+
+    /**
+     * Returns all nodes in the tree that match bn
+     * @param bn basename of node being searched for
+     */
+    public override findNodes(bn: string): Set<Node> {
+        this.assertIsValidBaseName(bn, ExceptionType.PRECONDITION);
+
+        const result: Set<Node> = new Set<Node>();
+        const children: Set<Node> = this.doGetChildNodes();
+        children.forEach((child) => {
+            if (child.getBaseName() == bn) {
+                result.add(child);
+            }
+            const grandChildren: Set<Node> = child.findNodes(bn);
+            grandChildren.forEach((grandChild) => {
+                result.add(grandChild);
+            });
+        });
+        if (this.getBaseName() == bn) {
+            result.add(this);
+        }
+
+        this.assertClassInvariants();
+        this.assertIsNotNullOrUndefined(result, ExceptionType.POSTCONDITION);
+        return result;
+    }
+
+    protected assertValidNode(cn: Node, et: ExceptionType): void {
+        this.assertIsNotNullOrUndefined(cn, et);
+        this.assertIsValidBaseName(cn.getBaseName(), et);
+    }
 }
--- src/adap-b05/files/Node.ts
+++ src/adap-b05/files/Node.ts
@@ -1,6 +1,4 @@
 import { ExceptionType, AssertionDispatcher } from "../common/AssertionDispatcher";
-import { IllegalArgumentException } from "../common/IllegalArgumentException";
-import { InvalidStateException } from "../common/InvalidStateException";
 
 import { Name } from "../names/Name";
 import { Directory } from "./Directory";
@@ -11,45 +9,97 @@ export class Node {
     protected parentNode: Directory;
 
     constructor(bn: string, pn: Directory) {
+        this.assertIsValidBaseName(bn, ExceptionType.PRECONDITION);
+
         this.doSetBaseName(bn);
         this.parentNode = pn; // why oh why do I have to set this
         this.initialize(pn);
+
+        this.assertClassInvariants();
     }
 
     protected initialize(pn: Directory): void {
+        this.assertIsNotNullOrUndefined(pn, ExceptionType.PRECONDITION);
+        let clone: Node = { ...this };
+
         this.parentNode = pn;
         this.parentNode.add(this);
+
+        this.assertClassInvariants();
+        try {
+            this.assertSuccessfulMove(pn, ExceptionType.POSTCONDITION);
+        } catch (e: any) {
+            this.parentNode.remove(this);
+            this.parentNode = clone.parentNode;
+            throw e;
+        }
     }
 
     public move(to: Directory): void {
+        this.assertIsNotNullOrUndefined(to, ExceptionType.PRECONDITION);
+        let clone: Node = { ...this };
+
         this.parentNode.remove(this);
         to.add(this);
         this.parentNode = to;
+
+        this.assertClassInvariants();
+        try {
+        this.assertSuccessfulMove(to, ExceptionType.POSTCONDITION);
+        } catch (e: any) {
+            to.remove(this);
+            this.parentNode = clone.parentNode;
+            this.parentNode.add(this);
+            throw e;
+        }
     }
 
     public getFullName(): Name {
         const result: Name = this.parentNode.getFullName();
-        result.append(this.getBaseName());
+        result.append(this.doGetBaseName());
+
+        this.assertClassInvariants();
+        this.assertIsNotNullOrUndefined(result, ExceptionType.POSTCONDITION);
         return result;
     }
 
     public getBaseName(): string {
-        return this.doGetBaseName();
-    }
+        const res: string = this.doGetBaseName();
 
+        this.assertClassInvariants();
+        this.assertIsNotNullOrUndefined(res, ExceptionType.POSTCONDITION);
+        return res;
+    }
     protected doGetBaseName(): string {
         return this.baseName;
     }
 
     public rename(bn: string): void {
-        this.doSetBaseName(bn);
-    }
+        this.assertIsValidBaseName(bn, ExceptionType.PRECONDITION);
+        let oldBn : string = this.getBaseName();
 
+        this.doSetBaseName(bn);
+
+        this.assertClassInvariants();
+        try {
+            this.assertIsValidBaseName(this.doGetBaseName(), ExceptionType.POSTCONDITION);
+        } catch (e: any) {
+            this.doSetBaseName(oldBn);
+            throw e;
+        }
+    }
     protected doSetBaseName(bn: string): void {
         this.baseName = bn;
     }
 
     public getParentNode(): Directory {
+        const res : Directory = this.doGetParentNode();
+
+        this.assertClassInvariants();
+        this.assertIsNotNullOrUndefined(res, ExceptionType.POSTCONDITION);
+        return res;
+    }
+    protected doGetParentNode(): Directory {
         return this.parentNode;
     }
 
@@ -58,17 +108,46 @@ export class Node {
      * @param bn basename of node being searched for
      */
     public findNodes(bn: string): Set<Node> {
-        throw new Error("needs implementation or deletion");
+        this.assertIsValidBaseName(bn, ExceptionType.PRECONDITION);
+
+        const result: Set<Node> = new Set<Node>();
+        if (this.getBaseName() == bn) {
+            result.add(this);
+        }
+
+        this.assertClassInvariants();
+        this.assertIsNotNullOrUndefined(result, ExceptionType.POSTCONDITION);
+        return result;
     }
 
     protected assertClassInvariants(): void {
         const bn: string = this.doGetBaseName();
+        const pd: Directory = this.doGetParentNode();
         this.assertIsValidBaseName(bn, ExceptionType.CLASS_INVARIANT);
+        this.assertIsNotNullOrUndefined(pd, ExceptionType.CLASS_INVARIANT);
+    }
+    protected assertIsNotNullOrUndefined(obj: any, et: ExceptionType): void {
+        const condition: boolean = (obj != null);
+        AssertionDispatcher.dispatch(et, condition, "null or undefined");
+    }
+
+    protected assertSuccessfulMove(to: Directory, et: ExceptionType): void {
+        this.assertIsNotNullOrUndefined(to, et);
+        this.assertIsNotNullOrUndefined(this.doGetParentNode(), et);
+        const condition: boolean = (this.doGetParentNode() == to);
+        let condition2: boolean = false;
+        to.getChildNodes().forEach((node) => {
+            if (node == this) {
+                condition2 = true;
+            }
+        });
+        AssertionDispatcher.dispatch(et, condition, "move failed");
+        AssertionDispatcher.dispatch(et, condition2, "move failed");
     }
 
     protected assertIsValidBaseName(bn: string, et: ExceptionType): void {
-        const condition: boolean = (bn != "");
-        AssertionDispatcher.dispatch(et, condition, "invalid base name");
+        const conditionEmpty: boolean = (bn != "");
+        AssertionDispatcher.dispatch(et, conditionEmpty, "invalid base name");
+        this.assertIsNotNullOrUndefined(bn, et);
     }
-
 }
